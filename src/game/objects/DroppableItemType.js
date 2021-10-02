633d55e89455f306fa7875f26409c5a5623fb096
Onni Rantanen <ounaifi@gmail.com>
2021-10-02 17:13:36 +0300

    Droppable item type scale

--- src/game/objects/DroppableItemType.js
+++ src/game/objects/DroppableItemType.js
@@ -3,32 +3,35 @@ import ImageResource from '/src/engine/resources/ImageResource';
 const droppableItemTypes = [];
 
 export default class DroppableItemType {
-  static SHIPPING_CONTAINER = new DroppableItemType(1, '/assets/shipping-container.png');
-  static SAFE = new DroppableItemType(1, '/assets/safe.png');
-  static WOODEN_CRATE = new DroppableItemType(1, '/assets/wooden-crate.png');
-  static CARDBOARD_BOX = new DroppableItemType(1, '/assets/cardboard-box.png');
-  static GRASS_BLOCK = new DroppableItemType(1, '/assets/grass-block.png');
-  static ROTARY_PHONE = new DroppableItemType(1, '/assets/rotary-phone.png');
-  static GIFT_BOX = new DroppableItemType(1, '/assets/gift-box.png');
-  static CRT_SCREEN = new DroppableItemType(1, '/assets/crt-screen.png');
-  static WASHING_MACHINE = new DroppableItemType(1, '/assets/washing-machine.png');
-  static WIDE_PAINTING = new DroppableItemType(1, '/assets/wide-painting.png');
-  static WIDE_PLANK = new DroppableItemType(1, '/assets/wide-plank.png');
+  static SHIPPING_CONTAINER = new DroppableItemType(1, 1, '/assets/shipping-container.png');
+  static SAFE = new DroppableItemType(1, 1, '/assets/safe.png');
+  static WOODEN_CRATE = new DroppableItemType(1, 1, '/assets/wooden-crate.png');
+  static CARDBOARD_BOX = new DroppableItemType(1, 1, '/assets/cardboard-box.png');
+  static GRASS_BLOCK = new DroppableItemType(1, 1, '/assets/grass-block.png');
+  static ROTARY_PHONE = new DroppableItemType(1, 1, '/assets/rotary-phone.png');
+  static GIFT_BOX = new DroppableItemType(1, 1, '/assets/gift-box.png');
+  static CRT_SCREEN = new DroppableItemType(1, 1, '/assets/crt-screen.png');
+  static WASHING_MACHINE = new DroppableItemType(1, 1, '/assets/washing-machine.png');
+  static WIDE_PAINTING = new DroppableItemType(1, 1, '/assets/wide-painting.png');
+  static WIDE_PLANK = new DroppableItemType(1, 1, '/assets/wide-plank.png');
 
   #mass;
+  #scale;
   #assetPath;
   #minHeight;
   #maxHeight;
 
   #resource = null;
 
-  constructor(mass, asset, minHeight = null, maxHeight = null) {
+  constructor(mass, scale, asset, minHeight = null, maxHeight = null) {
     if (typeof mass !== 'number' || isNaN(mass)) throw new Error(`Invalid mass ${mass}`);
+    if (typeof scale !== 'number' || isNaN(scale)) throw new Error(`Invalid scale ${scale}`);
     if (typeof asset !== 'string' || asset.length === 0) throw new Error(`Invalid asset name ${asset}`);
     if (isNaN(minHeight)) throw new Error('Min height is NaN');
     if (isNaN(maxHeight)) throw new Error('Min height is NaN');
 
     this.#mass = mass;
+    this.#scale = scale;
     this.#assetPath = asset;
     this.#minHeight = minHeight;
     this.#maxHeight = maxHeight;
