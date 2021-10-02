fe105beda6c31aaacf3966f03761b47e6ea191a1
Onni Rantanen <ounaifi@gmail.com>
2021-10-02 13:34:13 +0300

    Remove comment

--- src/engine/math/Vector2.js
+++ src/engine/math/Vector2.js
@@ -2,7 +2,6 @@ import Phaser from '/src/lib/phaser';
 
 export default class Vector2 extends Phaser.Math.Vector2 {
   get length() {
-    console.log('math', Phaser.Math);
     return Phaser.Math.Distance.Between(0, 0, this.x, this.y);
   }
 }
