c9db45522f5014983a19b8841a4cc245793cf37f
Onni Rantanen <ounaifi@gmail.com>
2021-10-02 13:17:02 +0300

    Have velocity return vector

--- src/engine/objects/MatterImage.js
+++ src/engine/objects/MatterImage.js
@@ -4,7 +4,7 @@ import Vector2 from '/src/engine/math/Vector2';
 
 export default class MatterImage extends Phaser.Physics.Matter.Image {
   get velocity() {
-    return new Vector2(this.body.velocity.x, this.body.velocity.y).length;
+    return new Vector2(this.body.velocity.x, this.body.velocity.y);
   }
 }
 
