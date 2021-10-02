2ce3301a03df179831a96db632e4101b962bd57e
Onni Rantanen <ounaifi@gmail.com>
2021-10-02 03:38:53 +0300

    Matter sprite and image objects

---
+++ src/engine/objects/MatterImage.js
@@ -0,0 +1,4 @@
+import Phaser from '/src/lib/phaser';
+
+export default class MatterImage extends Phaser.Physics.Matter.Image {}
+
---
+++ src/engine/objects/MatterSprite.js
@@ -0,0 +1,4 @@
+import Phaser from '/src/lib/phaser';
+
+export default class MatterSprite extends Phaser.Physics.Matter.Sprite {}
+
