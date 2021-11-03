d10ad9bfe42e0a908cc74c708d7071c880c5ce70
Onni Rantanen <ounai@users.noreply.github.com>
2021-11-03 10:14:02 +0200

    Update Game.js

--- src/engine/core/Game.js
+++ src/engine/core/Game.js
@@ -1,10 +1,7 @@
 import Phaser from '/src/lib/phaser';
 
-class Game extends Phaser.Game {
+export default class Game extends Phaser.Game {
   constructor(config) {
     super(config);
   }
 }
-
-export default Game;
-
